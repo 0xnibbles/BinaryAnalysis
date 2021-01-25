8dfdaf2870123cff1d438e74d77535363978e552
s4nkx0k <22789840+s4nkx0k@users.noreply.github.com>
2021-01-25 14:07:23 +0000

    loader first release

--- loader.h
+++ loader.h
@@ -3,12 +3,9 @@
 Messing a little bit with linux binaries and learning how at the low level things work.
 All the stuff here is made with the purpose to prepare myself for binary exploitation.
 
-> Learn how to build to know where to "destroy"
-
 ## Why binary Analysis?
 
 It's the art of analyzing binaries more specifically their properties, machine code and the data they have.
-Also, it's a challeging field because generally there is no symbolic information, no type information, no high level abstraction or data mixed with code.
+Also, it's a challeging field because generally there is no symbolic information, no type information, no high level abstraction or the presence of mixed code and data.
 
-(under development)
 
