fdd99bc62996f4ee5d13918e73a9502e28b6a675
s4nkx0k <22789840+s4nkx0k@users.noreply.github.com>
2021-02-01 14:08:53 +0000

    parsing binary properties

--- loader.cpp
+++ loader.cpp
@@ -51,3 +51,71 @@ static bfd* open_bfd(std::string &fname){
 
 }
 
+
+static int load_binary_bfd(std::string &fname, Binary *bin, Binary::Binary type){
+    int ret;
+    bfd *bfd_h;
+    const bfd_arch_info_type *bfd_info;
+
+    bfd_h= NULL;
+    bfd_h = open_bfd(fname);
+    if(!bfd_h){
+        goto fail;
+    }
+
+    bin->filename = std::string(fname);
+    bin->entry = bfd_get_start_address(bfd_h);
+    bin->type_str = std::string(bfd_h->xvec->name);
+
+    switch(bfd_h->xvec->flavour){
+
+        case bfd_target_elf_flavour:
+            bin->type = Binary::BIN_TYPE_ELF;
+            break;
+        case bfd_target_coff_flavour:
+            bin->type = Binary::BIN_TYPE_PE;
+            break;
+        case bfd_target_unknown_flavour:
+        default:
+            fprintf(stderr,"unsupported binary type (%s)\n", bfd_h->xvec->name);
+            goto fail;
+
+    }
+
+    bfd_info = bfd_get_arch_info(bfd_h);
+    bin->arch_str = std::string(bfd_info->printable_name);
+    switch(bfd_info->mach){
+        case bfd_mach_i386_i386:
+            bin->arch = Binary::ARCH_X86;
+            bin->bits = 32;
+            break;
+        case bfd_mach_x86_64:
+            bin->arch = Binary::ARCH_X86;
+            bin->bits = 64;
+            break;
+        default:
+            fprintf(stderr,"unsupported architecture (%s)\n", bfd_info->printable_name);
+            goto fail;
+    }
+
+    /* Symbol handling is best-effort only (they may not even be present) */
+    load_symbols_bfd(bfd_h, bin);
+    load_dynsym_bfd(bfd_h, bin);
+
+    if(load_sections_bfd(bfd_h, bin) < 0)
+        goto fail;
+
+    ret = 0;
+    goto cleanup;
+
+    fail:
+        ret = -1;
+
+    cleanup:
+        if(bfd_h)
+            bfd_close(bfd_h);
+        
+        return ret;
+
+}
+
